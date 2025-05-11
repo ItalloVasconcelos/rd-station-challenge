3224da7a6dfca2185edd1eb4d93ef278db445f08
Itallo Sávio <itallo.prog@gmail.com>
2025-05-11 19:38:55 -0300

    feat(product-service): Using mock data for deployment with deploy. Use of the Commented API

--- frontend/src/mocks/mockProducts.js
+++ frontend/src/mocks/mockProducts.js
@@ -1,63 +1,67 @@
 const mockProducts = [
   {
-    id: 1,
-    name: 'RD Station CRM',
-    category: 'Vendas',
-    preferences: [
-      'Integração fácil com ferramentas de e-mail',
-      'Personalização de funis de vendas',
-      'Relatórios avançados de desempenho de vendas',
+    "id": 1,
+    "name": "RD Station CRM",
+    "description": "Seu processo de vendas organizado e escalável",
+    "category": "Vendas",
+    "preferences": [
+      "Integração fácil com ferramentas de e-mail",
+      "Personalização de funis de vendas",
+      "Relatórios avançados de desempenho de vendas"
     ],
-    features: [
-      'Gestão de leads e oportunidades',
-      'Automação de fluxos de trabalho de vendas',
-      'Rastreamento de interações com clientes',
-    ],
+    "features": [
+      "Gestão de leads e oportunidades",
+      "Automação de fluxos de trabalho de vendas",
+      "Rastreamento de interações com clientes"
+    ]
   },
   {
-    id: 2,
-    name: 'RD Station Marketing',
-    category: 'Marketing',
-    preferences: [
-      'Automação de marketing',
-      'Testes A/B para otimização de campanhas',
-      'Segmentação avançada de leads',
+    "id": 2,
+    "name": "RD Station Marketing",
+    "description": "Automação de Marketing que cabe no bolso",
+    "category": "Marketing",
+    "preferences": [
+      "Automação de marketing",
+      "Testes A/B para otimização de campanhas",
+      "Segmentação avançada de leads"
     ],
-    features: [
-      'Criação e gestão de campanhas de e-mail',
-      'Rastreamento de comportamento do usuário',
-      'Análise de retorno sobre investimento (ROI) de campanhas',
-    ],
+    "features": [
+      "Criação e gestão de campanhas de e-mail",
+      "Rastreamento de comportamento do usuário",
+      "Análise de retorno sobre investimento (ROI) de campanhas"
+    ]
   },
   {
-    id: 3,
-    name: 'RD Conversas',
-    category: 'Omnichannel',
-    preferences: [
-      'Integração com chatbots',
-      'Histórico unificado de interações',
-      'Respostas automáticas e personalizadas',
+    "id": 3,
+    "name": "RD Conversas",
+    "description": "Atendimento digital ágil e em um só lugar",
+    "category": "Omnichannel",
+    "preferences": [
+      "Integração com chatbots",
+      "Histórico unificado de interações",
+      "Respostas automáticas e personalizadas"
     ],
-    features: [
-      'Gestão de conversas em diferentes canais',
-      'Chat ao vivo e mensagens automatizadas',
-      'Integração com RD Station CRM e Marketing',
-    ],
+    "features": [
+      "Gestão de conversas em diferentes canais",
+      "Chat ao vivo e mensagens automatizadas",
+      "Integração com RD Station CRM e Marketing"
+    ]
   },
   {
-    id: 4,
-    name: 'RD Mentor AI',
-    category: 'Uso de Inteligência Artificial',
-    preferences: [
-      'Análise preditiva de dados',
-      'Recomendações personalizadas para usuários',
-      'Integração com assistentes virtuais',
+    "id": 4,
+    "name": "RD Mentor AI",
+    "description": "Mentoria com AI, para facilitar seus processos.",
+    "category": "Uso de Inteligência Artificial",
+    "preferences": [
+      "Análise preditiva de dados",
+      "Recomendações personalizadas para usuários",
+      "Integração com assistentes virtuais"
     ],
-    features: [
-      'Análise de dados para insights estratégicos',
-      'Recomendação de ações com base em padrões',
-      'Integração de funcionalidades preditivas nos produtos RD Station',
-    ],
+    "features": [
+      "Análise de dados para insights estratégicos",
+      "Recomendação de ações com base em padrões",
+      "Integração de funcionalidades preditivas nos produtos RD Station"
+    ]
   },
 ];
 
--- frontend/src/services/product.service.js
+++ frontend/src/services/product.service.js
@@ -1,15 +1,28 @@
-import axios from 'axios';
+// import axios from 'axios';
+//
+// const baseURL = 'http://localhost:3001';
+//
+// const getProducts = async () => {
+//   try {
+//     const response = await axios.get(`${baseURL}/products`);
+//     return response.data;
+//   } catch (error) {
+//     console.error('Erro ao obter os produtos:', error);
+//     throw error;
+//   }
+// };
 
-const baseURL = 'http://localhost:3001';
+import mockProducts from '../mocks/mockProducts';
 
-const getProducts = async () => {
+export async function getProducts() {
   try {
-    const response = await axios.get(`${baseURL}/products`);
-    return response.data;
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/products`);
+    if (!response.ok) throw new Error('API Offline');
+    return await response.json();
   } catch (error) {
-    console.error('Erro ao obter os produtos:', error);
-    throw error;
+    console.warn('Usando mocks: API Offline ou indisponível.');
+    return mockProducts;
   }
-};
+}
 
 export default getProducts;
